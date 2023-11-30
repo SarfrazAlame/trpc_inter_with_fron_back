e343380854a1a9358a2aeab9d51bf5e65bc85751
Sarfraz Alam <sarfrazk7858@gmail.com>
2023-11-30 08:37:25 +0530

    practice

--- Practice/server/index.ts
+++ Practice/server/index.ts
@@ -3,23 +3,6 @@ import { z } from 'zod'
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
 
 const appRouter = router({
-    // createTodo: publicProcedure
-    //     .input(z.object({
-    //         title: z.string(),
-    //         description: z.string(),
-    //     }))
-    //     .mutation(async (opts) => {
-    //         console.log("Hii there")
-    //         const title = opts.input.title
-    //         const description = opts.input.description
-    //         // db stuff here
-
-    //         return {
-    //             id: "1",
-    //             title,
-    //             description,
-    //         }
-    //     }),
     signUp: publicProcedure
         .input(z.object({
             email: z.string(),
